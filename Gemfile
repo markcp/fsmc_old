cbc0671b649527b3c7b0aa18a4ea5e22d8c2381b
Mark Pittillo <mark-pittillo@uiowa.edu>
2012-10-22 21:15:15 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'jquery-rails', '2.1.3'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.6'
+  gem 'rspec-rails', '2.11.4'
+end
+
+group :assets do
+  gem 'sass-rails', '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.3.0'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skandies::Application.config.secret_token = '51f774ccc39797ff9a096adca314a64ccce4e7ea4cb338aa5c0edb3760c52c9f997c42239fa8250f327203b957f0e99719e9333dbe70b96135493ab0610f3306'
